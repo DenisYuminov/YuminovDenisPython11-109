f52166acf8d90ba1bb478129089c6ba4841a8a99
DenisYuminov <uminovdenis123@gmail.com>
2022-10-04 22:06:41 +0300

    hw3 done

---
+++ hw/hw3/application.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+counter = 0
+
+@app.route("/")
+@app.route("/products")
+def hello():
+    return render_template("products.html")
+
+@app.route("/test")
+def zero():
+    return render_template("404.html")
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=10008, debug=True)
